6e69d358d6078df21349fee294e5a9221241d747
HamidDonyavirad <donyavirad76@gmail.com>
2025-05-22 20:49:03 +0330

    feate: create models and Displaying models in the Django admin panel

--- courseResSys/courses/admin.py
+++ courseResSys/courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Reservation, Course
+from django.contrib.auth import get_user_model
 # Register your models here.
+admin.site.register(get_user_model())
+admin.site.register(Course)
+admin.site.register(Reservation)
--- courseResSys/courses/models.py
+++ courseResSys/courses/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User (AbstractUser):
+    ROLE_CHOISES = (
+        ('student','Student'),
+        ('instructor', 'Instructor'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=20,choices=ROLE_CHOISES) 
+    
+class Course (models.Model):
+    title = models.CharField(max_length=200)  
+    description = models.TextField()
+    instructor = models.ForeignKey (User, on_delete=models.CASCADE, limit_choices_to={'role':'instructor'})
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    capacity = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return self.title
+    
+class Reservation (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(User,on_delete=models.CASCADE) 
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user','course')
+             
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.course.title}"
