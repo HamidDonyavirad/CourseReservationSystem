eddb7295382d3d272f3d9c2a9d4d83278fe96a75
HamidDonyavirad <donyavirad76@gmail.com>
2025-05-29 21:01:33 +0330

    feat: add link to course page

--- courseResSys/courses/views.py
+++ courseResSys/courses/views.py
@@ -11,12 +11,14 @@ from django.contrib.auth.forms import AuthenticationForm
 #Show course list (Course)
 def course_list (request):
     courses = Course.objects.all()
-    return render(request,'courses/course_list.html', {'courses': courses})
+    return render(request,'home.html', {'courses': courses})
 
 #Show details of each course
 def course_detail(request,course_id):
    course = get_object_or_404(Course,id = course_id) 
-   is_reserved = Reservation.objects.filter(user=request.user, course=course).exists()
+   is_reserved = False
+   if request.user.is_authenticated:
+        is_reserved =Reservation.objects.filter(user=request.user, course=course).exists()
    return render(request, 'courses/course_detail.html', {
         'course': course,
         'is_reserved': is_reserved,
@@ -35,7 +37,7 @@ def reserve_course(request, course_id):
                 messages.success(request, 'Reservation was successful.')
             except:
                 messages.error(request,'You have already booked this course.')
-            return redirect('course_detial', course_id=course.id)
+            return redirect('course_detail', course_id=course.id)
     else:
         form = ReservationForm(user = request.user, course=course)
     return render(request, 'courses/reserve_course.html', {
